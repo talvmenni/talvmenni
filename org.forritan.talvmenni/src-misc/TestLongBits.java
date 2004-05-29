22944950548d8866ae94e3574bff86e3f1072bbb
Eyðun Nielsen <eydun_n@users.sourceforge.net>
2004-05-29 17:09:37 +0000

    Initial

---
+++ org.forritan.talvmenni/src-misc/TestLongBits.java
@@ -0,0 +1,11 @@
+public class TestLongBits {
+
+   public static void main(String[] args) {
+      
+      System.out.println("Testing the bits of the longs:");
+      System.out.println();
+      for (long i= -10L; i < 11L; i++) {
+         System.out.println(" - " + i + "L: " + Long.toBinaryString(i) );         
+      }      
+   }
+}
