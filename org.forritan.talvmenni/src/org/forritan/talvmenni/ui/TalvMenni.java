2bd9e41042e8c9f4f46ff84b5539b6b117466bd0
Eyðun Nielsen <eydun_n@users.sourceforge.net>
2004-06-14 19:53:25 +0000

    Minor update

--- org.forritan.talvmenni/src/org/forritan/talvmenni/ui/TalvMenni.java
+++ org.forritan.talvmenni/src/org/forritan/talvmenni/ui/TalvMenni.java
@@ -1,19 +1,15 @@
 package org.forritan.talvmenni.ui;
 
 public class TalvMenni {
-   public TalvMenni(String args[]) {
-      if (args.length > 0
-            && args[0].charAt(0) == 'x') {
-         XboardMode Xboard= new XboardMode();
-         Xboard.create();
-      } else {
-         ConsoleMode Console= new ConsoleMode();
-         Console.create();
-      }
-
-   }
 
    public static void main(String args[]) {
-      TalvMenni TalvMenni= new TalvMenni(args);
+      if (args.length > 0
+            && args[0].equalsIgnoreCase("x")) {
+         XboardMode xBoard= new XboardMode();
+         xBoard.create();
+      } else {
+         ConsoleMode console= new ConsoleMode();
+         console.create();
+      }
    }
 }
