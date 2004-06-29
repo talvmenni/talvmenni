58cd011623366434a8b1c11920eb006a38622e7c
Eyðun Nielsen <eydun_n@users.sourceforge.net>
2004-06-29 13:19:11 +0000

    Initial

---
+++ org.forritan.talvmenni/src/org/forritan/talvmenni/bitboard/paths/BlackEnpassant.java
@@ -0,0 +1,87 @@
+package org.forritan.talvmenni.bitboard.paths;
+
+import org.forritan.talvmenni.bitboard.Square;
+
+
+public interface BlackEnpassant extends Enpassant {
+
+   public static final long _PATHS_FROM_A8 = 0L;
+   public static final long _PATHS_FROM_B8 = 0L;
+   public static final long _PATHS_FROM_C8 = 0L;
+   public static final long _PATHS_FROM_D8 = 0L;
+   public static final long _PATHS_FROM_E8 = 0L;
+   public static final long _PATHS_FROM_F8 = 0L;
+   public static final long _PATHS_FROM_G8 = 0L;
+   public static final long _PATHS_FROM_H8 = 0L;
+   public static final long _PATHS_FROM_A7 = Square._B6
+                                                 | Square._A5;
+   public static final long _PATHS_FROM_B7 = Square._A6
+                                                 | Square._C6
+                                                 | Square._B5;
+   public static final long _PATHS_FROM_C7 = Square._B6
+                                                 | Square._D6
+                                                 | Square._C5;
+   public static final long _PATHS_FROM_D7 = Square._C6
+                                                 | Square._E6
+                                                 | Square._D5;
+   public static final long _PATHS_FROM_E7 = Square._D6
+                                                 | Square._F6
+                                                 | Square._E5;
+   public static final long _PATHS_FROM_F7 = Square._E6
+                                                 | Square._G6
+                                                 | Square._F5;
+   public static final long _PATHS_FROM_G7 = Square._F6
+                                                 | Square._H6
+                                                 | Square._G5;
+   public static final long _PATHS_FROM_H7 = Square._G6
+                                                 | Square._H5;
+   public static final long _PATHS_FROM_A6 = 0L;
+   public static final long _PATHS_FROM_B6 = 0L;
+   public static final long _PATHS_FROM_C6 = 0L;
+   public static final long _PATHS_FROM_D6 = 0L;
+   public static final long _PATHS_FROM_E6 = 0L;
+   public static final long _PATHS_FROM_F6 = 0L;
+   public static final long _PATHS_FROM_G6 = 0L;
+   public static final long _PATHS_FROM_H6 = 0L;
+   public static final long _PATHS_FROM_A5 = 0L;
+   public static final long _PATHS_FROM_B5 = 0L;
+   public static final long _PATHS_FROM_C5 = 0L;
+   public static final long _PATHS_FROM_D5 = 0L;
+   public static final long _PATHS_FROM_E5 = 0L;
+   public static final long _PATHS_FROM_F5 = 0L;
+   public static final long _PATHS_FROM_G5 = 0L;
+   public static final long _PATHS_FROM_H5 = 0L;
+   public static final long _PATHS_FROM_A4 = 0L;
+   public static final long _PATHS_FROM_B4 = 0L;
+   public static final long _PATHS_FROM_C4 = 0L;
+   public static final long _PATHS_FROM_D4 = 0L;
+   public static final long _PATHS_FROM_E4 = 0L;
+   public static final long _PATHS_FROM_F4 = 0L;
+   public static final long _PATHS_FROM_G4 = 0L;
+   public static final long _PATHS_FROM_H4 = 0L;
+   public static final long _PATHS_FROM_A3 = 0L;
+   public static final long _PATHS_FROM_B3 = 0L;
+   public static final long _PATHS_FROM_C3 = 0L;
+   public static final long _PATHS_FROM_D3 = 0L;
+   public static final long _PATHS_FROM_E3 = 0L;
+   public static final long _PATHS_FROM_F3 = 0L;
+   public static final long _PATHS_FROM_G3 = 0L;
+   public static final long _PATHS_FROM_H3 = 0L;
+   public static final long _PATHS_FROM_A2 = 0L;
+   public static final long _PATHS_FROM_B2 = 0L;
+   public static final long _PATHS_FROM_C2 = 0L;
+   public static final long _PATHS_FROM_D2 = 0L;
+   public static final long _PATHS_FROM_E2 = 0L;
+   public static final long _PATHS_FROM_F2 = 0L;
+   public static final long _PATHS_FROM_G2 = 0L;
+   public static final long _PATHS_FROM_H2 = 0L;
+   public static final long _PATHS_FROM_A1 = 0L;
+   public static final long _PATHS_FROM_B1 = 0L;
+   public static final long _PATHS_FROM_C1 = 0L;
+   public static final long _PATHS_FROM_D1 = 0L;
+   public static final long _PATHS_FROM_E1 = 0L;
+   public static final long _PATHS_FROM_F1 = 0L;
+   public static final long _PATHS_FROM_G1 = 0L;
+   public static final long _PATHS_FROM_H1 = 0L;
+
+}
---
+++ org.forritan.talvmenni/src/org/forritan/talvmenni/bitboard/paths/Enpassant.java
@@ -0,0 +1,4 @@
+package org.forritan.talvmenni.bitboard.paths;
+
+public interface Enpassant extends Piece {
+}
