9db0ba9b335a8fab00a0ae22ffa6c46c17fe332a
Eyðun Nielsen <eydun_n@users.sourceforge.net>
2004-07-16 14:53:53 +0000

    Update

--- org.forritan.talvmenni/src-misc/TestSpeedOnMakeRandomMove.java
+++ org.forritan.talvmenni/src-misc/TestSpeedOnMakeRandomMove.java
@@ -5,16 +5,27 @@ public class TestSpeedOnMakeRandomMove {
    public static void main(
          String[] args) {
 
+      int warmup= 100000;
+     
+      Position p= Position.createInitial();      
+      for (int i= 0; i < warmup; i++) {
+         Position.Move move= p.getRandomMove((i % 2) == 0);
+         if(move != null) {
+            p= p.move(move.from, move.to);
+         } else {
+            p= Position.createInitial();
+         }
+      }
+      
+      
+      
+      p= Position.createInitial();
       int howMany= 1000000;
-
       System.out.println("Making "
             + howMany
             + " random moves...");
-      
-      System.out.println();
-      
+            
 
-      Position p= Position.createInitial();
       long time= -System.currentTimeMillis();
       for (int i= 0; i < howMany; i++) {
          Position.Move move= p.getRandomMove((i % 2) == 0);
@@ -27,8 +38,6 @@ public class TestSpeedOnMakeRandomMove {
       }
 
       time+= System.currentTimeMillis();
-
-      System.out.println();
 
       System.out.println("...in "
             + time
