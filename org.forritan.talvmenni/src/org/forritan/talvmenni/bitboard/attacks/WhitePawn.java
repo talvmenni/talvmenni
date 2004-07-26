0c60bf2adfb22b8b365105180cc0e2436e89fd2a
Eyðun Nielsen <eydun_n@users.sourceforge.net>
2004-07-26 17:14:33 +0000

    Update

--- org.forritan.talvmenni/src/org/forritan/talvmenni/bitboard/attacks/WhitePawn.java
+++ org.forritan.talvmenni/src/org/forritan/talvmenni/bitboard/attacks/WhitePawn.java
@@ -8,22 +8,25 @@ import org.forritan.talvmenni.game.Position;
 
 public class WhitePawn {
 
-   public static long captureMoveAttacksFrom(long square, Position p) {
+   public static long captureMoveAttacksFrom(
+         long square,
+         Position p) {
       long result= 0L;
-      result= WhitePawnCaptures.create().getPathsFrom(square);
+      result= WhitePawnCaptures.create().getPathsFrom(
+            square);
       result&= ~p.white.allPieces;
       result&= p.black.allPieces;
       return result;
    }
 
-   public static long moveAttacksFrom(long square, Position p) {
-      long result= 0L;
-      result= WhitePawnMoves.create().getPathsFrom(square);
+   public static long moveAttacksFrom(
+         long square,
+         Position p) {
+      long result= WhitePawnMoves.create().getPathsFrom(
+            square);
       result&= ~p.black.allPieces;
       result&= ~p.white.allPieces;
-      if ((square & Rank._2) != 0L) {
-         result&= ~(((p.white.allPieces | p.black.allPieces) & Rank._3) << 8);
-      }
+      result&= ~(((p.white.allPieces | p.black.allPieces) & Rank._3 & (square << 8)) << 8);
       return result;
    }
 }
