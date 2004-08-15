b46b69fea67a8a975fc6f02dace01f674b72613f
Eyðun Nielsen <eydun_n@users.sourceforge.net>
2004-08-15 19:21:49 +0000

    Update

--- org.forritan.talvmenni/src/org/forritan/talvmenni/game/Transposition.java
+++ org.forritan.talvmenni/src/org/forritan/talvmenni/game/Transposition.java
@@ -1,7 +1,6 @@
 package org.forritan.talvmenni.game;
 
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,18 +9,18 @@ import org.forritan.talvmenni.game.Position.Move;
 
 public class Transposition {
 
-   private Map<Position, Entry> whiteTable;
-   private Map<Position, Entry> blackTable;
+   private Table<Position, Entry> whiteTable;
+   private Table<Position, Entry> blackTable;
 
    public Transposition() {
-      this.whiteTable= new HashMap<Position, Entry>();
-      this.blackTable= new HashMap<Position, Entry>();
+      this.whiteTable= new Table<Position, Entry>();
+      this.blackTable= new Table<Position, Entry>();
    }
 
    public static class Entry {
       public List<Move> moves;
-      public int      score;
-      public int      ply;
+      public int        score;
+      public int        ply;
 
       private Entry(
             List<Move> moves,
@@ -33,16 +32,18 @@ public class Transposition {
       }
    }
 
-   public int size(boolean white) {
-      if(white) {
+   public int size(
+         boolean white) {
+      if (white) {
          return this.whiteTable.size();
       } else {
          return this.blackTable.size();
       }
    }
 
-   public boolean isEmpty(boolean white) {
-      if(white) {
+   public boolean isEmpty(
+         boolean white) {
+      if (white) {
          return this.whiteTable.isEmpty();
       } else {
          return this.blackTable.isEmpty();
@@ -50,8 +51,9 @@ public class Transposition {
    }
 
    public boolean contains(
-         Position p, boolean white) {
-      if(white) {
+         Position p,
+         boolean white) {
+      if (white) {
          return this.whiteTable.containsKey(p);
       } else {
          return this.blackTable.containsKey(p);
@@ -59,8 +61,9 @@ public class Transposition {
    }
 
    public Entry get(
-         Position p, boolean white) {
-      if(white) {
+         Position p,
+         boolean white) {
+      if (white) {
          return this.whiteTable.get(p);
       } else {
          return this.blackTable.get(p);
@@ -74,39 +77,65 @@ public class Transposition {
          int score,
          int ply) {
 
-      if(white) {
-         if(this.whiteTable.containsKey(position)) {
+      if (white) {
+         if (this.whiteTable.containsKey(position)) {
             Entry e= this.whiteTable.get(position);
-            if(e.ply < ply || (e.ply == ply && e.score < score)) {
+            if (e.ply < ply
+                  || (e.ply == ply && e.score < score)) {
                e.ply= ply;
                e.score= score;
                e.moves.clear();
                e.moves.addAll(moves);
             }
          } else {
-            this.whiteTable.put(position.getImmutable(), new Entry(moves, score, ply));
+            this.whiteTable.put(
+                  position.getImmutable(),
+                  new Entry(
+                        moves,
+                        score,
+                        ply));
          }
       } else {
-         if(this.blackTable.containsKey(position)) {
+         if (this.blackTable.containsKey(position)) {
             Entry e= this.blackTable.get(position);
-            if(e.ply > ply || (e.ply == ply && e.score > score)) {
+            if (e.ply > ply
+                  || (e.ply == ply && e.score > score)) {
                e.ply= ply;
                e.score= score;
                e.moves.clear();
                e.moves.addAll(moves);
             }
          } else {
-            this.blackTable.put(position.getImmutable(), new Entry(moves, score, ply));
+            this.blackTable.put(
+                  position.getImmutable(),
+                  new Entry(
+                        moves,
+                        score,
+                        ply));
          }
-      }      
+      }
    }
 
-   public void clear(boolean white) {
-      if(white) {
+   public void clear(
+         boolean white) {
+      if (white) {
          this.whiteTable.clear();
       } else {
          this.blackTable.clear();
       }
    }
+
+   private static class Table<K, V> extends LinkedHashMap<K, V> implements
+         Map<K, V> {
+      private static final long serialVersionUID = 1L;
+      private static final int  MAX_ENTRIES      = 20000;
+
+      protected boolean removeEldestEntry(
+            Map.Entry eldest) {
+         return size() > MAX_ENTRIES;
+      }
+
+   }
+   //   
 }
 
